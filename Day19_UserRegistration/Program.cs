1c81ddfd3026f32cdc97da1daf407a66d3bb8fa8
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-03-02 17:15:40 +0530

    Checking first name using regular expression UC1_FirstName

--- Day19_UserRegistration/Program.cs
+++ Day19_UserRegistration/Program.cs
@@ -7,6 +7,15 @@ namespace Day19_UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the regular expression");
+            Console.WriteLine("1.First Name");
+            Console.WriteLine("Choose option from above");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    UserDetails.Check_First_Name();
+                    break;
+            }
         }
     }
 }
---
+++ Day19_UserRegistration/UserDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19_UserRegistration
+{
+    public class UserDetails
+    {
+        public static void Check_First_Name()
+        {
+            string pattern = "^[A-Z]{1}[a-z]{2,}$";
+            string[] names = { "Ram", "Suraj", "Shweta", "Manish", "Rahul", "Siva","vaibhav" };
+            foreach(string Name in names)
+            {
+                if(Regex.IsMatch(Name , pattern))
+                {
+                    Console.WriteLine("{0} is valid",Name);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not valid", Name);
+                }
+            }
+        }
+    }
+}
