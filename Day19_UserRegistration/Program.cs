074e741a5cadc7daf64eddc193c849d4039931b6
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-03-02 16:59:04 +0530

    Adding welcome message

---
+++ Day19_UserRegistration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day19_UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the regular expression");
+        }
+    }
+}
